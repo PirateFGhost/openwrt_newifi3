a7dc72dd6be8d2fd4ad3a322fee3c7d4415de56c
PirateFGhost <65862115+PirateFGhost@users.noreply.github.com>
2020-05-25 10:19:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,5 +11,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+ Add a feed source
+ git clone https://github.com/jerrykuku/lua-maxminddb.git  #git lua-maxminddb 依赖
+
+git clone https://github.com/jerrykuku/luci-app-vssr.git  
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
